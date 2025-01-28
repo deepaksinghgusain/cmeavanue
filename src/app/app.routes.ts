15479bc1ab1dce914ccdf9c2767a886d7d947cac
unknown <deepaksinghgusain>
2025-01-28 22:42:54 +0530

    login ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,13 @@ export const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }, 
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
