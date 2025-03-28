520a53935e58907e363d9759ca5fa48c98f27e8c
unknown <deepaksinghgusain>
2025-03-28 17:33:29 +0530

    fixed app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,17 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'course/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { slug: "home" },
+        { slug: "about" },
+        { slug: "contact" },
+      ];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
